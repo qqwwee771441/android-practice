e74ac67dda5b50bd2f66fb4874270db623ebdb4b
qqwwee771441 <104188251+qqwwee771441@users.noreply.github.com>
2024-05-07 19:46:51 +0900

    Add files via upload

--- week10_skeleton/week10_1/MainActivity.kt
+++ week10_skeleton/week10_1/MainActivity.kt
@@ -1,35 +1,40 @@
-package com.example.week10_1
+package com.choi.myapplication
 
+import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.* // id를 바로 사용할 수 있도록 import
+import com.choi.myapplication.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     var database: SQLiteDatabase? = null
+    private lateinit var binding:ActivityMainBinding
+    val databaseName = "people"
+    val tableName = "student"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        doButton1.setOnClickListener{
+        binding.doButton1.setOnClickListener{
             createDatabase()
         }
         // TODO : 버튼 이벤트 처리
-        doButton2.setOnClickListener{
+        binding.doButton2.setOnClickListener{
             createTable()
         }
-        doButton3.setOnClickListener{
+        binding.doButton3.setOnClickListener{
             addData()
         }
-        doButton4.setOnClickListener{
+        binding.doButton4.setOnClickListener{
             updateData()
         }
-        doButton5.setOnClickListener{
+        binding.doButton5.setOnClickListener{
             queryData()
         }
-        doButton6.setOnClickListener{
+        binding.doButton6.setOnClickListener{
             deleteData()
         }
     }
@@ -37,56 +42,55 @@ class MainActivity : AppCompatActivity() {
     //데이터 베이스 생성
     fun createDatabase(){
         // TODO : database 생성
-        database = openOrCreateDatabase("people", MODE_PRIVATE, null)
-        output1.append("데이터베이스 생성 또는 오픈함\n")
+        database = openOrCreateDatabase(databaseName, MODE_PRIVATE, null)
+        binding.output1.append("데이터베이스 생성 또는 오픈함\n")
     }
 
     //테이블 생성
     fun createTable(){
         if(database == null){
-            output1.append("데이터베이스를 먼저 오픈하세요.\n")
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
             return
         }
         // TODO : table 생성
-        database?.execSQL("create table if not exists student (id integer PRIMARY KEY autoincrement, name text, age integer, mobile text)")
-        output1.append("테이블 생성함\n")
+        database?.execSQL("create table if not exists $tableName (id integer PRIMARY KEY autoincrement, name text, age integer, mobile text)")
+        binding.output1.append("테이블 생성함\n")
     }
 
     //데이터 추가
     fun addData(){
         if (database == null){
-            output1.append("데이터베이스를 먼저 오픈하세요.\n")
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
             return
         }
         // TODO : 데이터 추가
-        database?.execSQL("insert into student (name, age, mobile) values ('john', '20', '010-0000-0000')")
-        output1.append("데이터 추가함\n")
+        database?.execSQL("insert into $tableName (name, age, mobile) values ('john', '20', '010-0000-0000')")
+        binding.output1.append("데이터 추가함\n")
     }
 
     fun updateData() {
         if (database == null){
-            output1.append("데이터베이스를 먼저 오픈하세요.\n")
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
             return
         }
 
-        //database = openOrCreateDatabase("people", MODE_PRIVATE, null)
-        var values
+        val values = ContentValues()
         values.put("name", "mike")
         values.put("age", "24")
         values.put("mobile", "010-4000-4000")
         var arr : Array<String> = arrayOf("john")
-        database?.update("student", values, "name=?", arr)
-        output1.append("데이터 갱신\n")
+        database?.update(tableName, values, "name=?", arr)
+        binding.output1.append("데이터 갱신\n")
     }
 
     //데이터 조회
     fun queryData(){
         if(database == null){
-            output1.append("데이터베이스를 먼저 오픈하세요.\n")
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
             return
         }
         // TODO : 데이터 조회
-        val cursor = database?.rawQuery("select _id, name, age, mobile from student", null)
+        val cursor = database?.rawQuery("select id, name, age, mobile from $tableName", null)
         if (cursor != null) {
             for (index in 0 until cursor.count) {
                 cursor.moveToNext()
@@ -94,29 +98,29 @@ class MainActivity : AppCompatActivity() {
                 val name = cursor.getString(1)
                 val age = cursor.getInt(2)
                 val mobile = cursor.getString(3)
-                output1.append("레코드#${index} : $id, $name, $age, $mobile\n")
+                binding.output1.append("레코드#${index} : $id, $name, $age, $mobile\n")
             }
             cursor.close()
         }
-        output1.append("데이터 조회 결과\n")
+        binding.output1.append("데이터 조회 결과\n")
     }
 
     fun deleteData() {
         if(database == null){
-            output1.append("데이터베이스를 먼저 오픈하세요.\n")
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
             return
         }
 
-        val sql = "select _id, name, age, mobile from student"
+        val sql = "select id, name, age, mobile from $tableName"
         val cursor = database?.rawQuery(sql, null)
         if (cursor != null) {
             cursor.count
             val count = cursor.count
             cursor.close()
 
-            val delete = "delete from student where _id = ${count}"            
+            val delete = "delete from $tableName where id = ${count}"
             database?.execSQL(delete)
-            output1.append("데이터 삭제\n")
+            binding.output1.append("데이터 삭제\n")
         }
     }
 }
--- week10_skeleton/week10_2/MainActivity.kt
+++ week10_skeleton/week10_2/MainActivity.kt
@@ -1,151 +1,126 @@
-package org.example.week10_2
+package com.choi.myapplication
 
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.Intent
+import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_main.*
-import java.io.*
+import androidx.appcompat.app.AppCompatActivity
+import com.choi.myapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    val databaseName = "movie0"
     var database: SQLiteDatabase? = null
-    val tableName = "movie_reserved"
+    private lateinit var binding:ActivityMainBinding
+    val databaseName = "people"
+    val tableName = "student"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        createDatabase()
-        createTable()
-
+        binding.doButton1.setOnClickListener{
+            createDatabase()
+        }
         // TODO : 버튼 이벤트 처리
-        savebutton.addEventListener{
-            saveMovie()
+        binding.doButton2.setOnClickListener{
+            createTable()
         }
-        viewbutton.addEventListener{
-            loadMovie()
+        binding.doButton3.setOnClickListener{
+            addData()
+        }
+        binding.doButton4.setOnClickListener{
+            updateData()
+        }
+        binding.doButton5.setOnClickListener{
+            queryData()
+        }
+        binding.doButton6.setOnClickListener{
+            deleteData()
         }
     }
 
-    fun saveMovie(){
-        val posterImageUri = savePosterToFile(R.drawable.dd)
-
-        // TODO : record에 저장할 데이터를 위젯으로부터 가져와 데이터베이스에 추가
-        val name = input1.text
-        val poster_image = posterImageView
-        val director = input3.text
-        val synopsis = input4.text
-        val reserved_time = input2.text
-
-        addData(name,poster_image,director,synopsis,reserved_time)
-    }
-
-    fun savePosterToFile(drawable:Int):Uri{
-        val drawable = ContextCompat.getDrawable(applicationContext,drawable)
-        val bitmap = (drawable as BitmapDrawable).bitmap
-
-        val wrapper = ContextWrapper(applicationContext)
-        val imagesFolder = wrapper.getDir("images", Context.MODE_PRIVATE)
-        val file = File(imagesFolder,"dd.jpg")
-
-        try{
-            val stream: OutputStream = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream)
-            stream.flush()
-            stream.close()
-        }catch (e:IOException){
-            e.printStackTrace()
-        }
-        return Uri.parse(file.absolutePath)
-    }
-
-    fun addData(name:String, poster_image:String, director:String, synopsis:String, reserved_time:String){
-        if(database==null){
-            println("데이터베이스를 먼저 오픈하세요\n")
-            return
-        }
-        // TODO : 데이터 추가 sql문 작성 및 실행, INSERT INTO sql문을 사용
-        database.execSQL("insert into ${tableName} (name, poster_image, director, synopsis, reserved_time) values ($name, $poster_image, $director, $synopsis, $reserved_time)")
-        println("데이터 추가함\n")
-    }
-
+    //데이터 베이스 생성
     fun createDatabase(){
         // TODO : database 생성
         database = openOrCreateDatabase(databaseName, MODE_PRIVATE, null)
-        println("데이터베이스 생성 또는 오픈함")
+        binding.output1.append("데이터베이스 생성 또는 오픈함\n")
     }
 
+    //테이블 생성
     fun createTable(){
-        val sql = "create table if not exists ${tableName}"+
-                "(_id integer PRIMARY KEY autoincrement, "+
-                "name text, "+
-                "poster_image text, "+
-                "director text, "+
-                "synopsis text, "+
-                "reserved_time text)"
-
         if(database == null){
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
             return
         }
-        // TODO : sql 실행
-        database?.execSQL(sql)
-        println("데이터베이스 생성\n")
+        // TODO : table 생성
+        database?.execSQL("create table if not exists $tableName (id integer PRIMARY KEY autoincrement, name text, age integer, mobile text)")
+        binding.output1.append("테이블 생성함\n")
     }
 
-    fun loadMovie(){
-        val movies = queryData()
-
-        // TODO : query로 조회한 데이터를 ReservedActivity로 전달
-        Intent intent = new Intent(this, ReservedActivity.class)
-        intent.putExtra("movies", movies)
-        startActivity(intent)
-    }
-
-    fun queryData():ArrayList<ReservedMovie>?{
-        val sql = "select _id, name, poster_image, director, synopsis, reserved_time from ${tableName}"
-
-        if(database == null){
-            println("데이터베이스를 먼저 오픈하세요.\n")
-            return null
+    //데이터 추가
+    fun addData(){
+        if (database == null){
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
+            return
         }
-        val list= arrayListOf<ReservedMovie>()
-        val cursor = database?.rawQuery(sql,null)
-        if(cursor!=null){
-            for (index in 0 until cursor.count){
-                cursor.moveToNext()
-                // TODO : 레코드에서 데이터 추출
-                val _id = cursor.getInt(0)
-                val name = cursor.getString(1)
-                val poster_image = cursor.getString(2)
-                val director = cursor.getString(3)
-                val synopsis = cursor.getString(4)
-                val reserved_time = cursor.getString(5)
-                println("레코드# ${index}: $_id, $name, $poster_image, $director, $synopsis, $reserved_time\n")
+        // TODO : 데이터 추가
+        database?.execSQL("insert into $tableName (name, age, mobile) values ('john', '20', '010-0000-0000')")
+        binding.output1.append("데이터 추가함\n")
+    }
 
-                val movie = ReservedMovie(_id,name,poster_image,director,synopsis,reserved_time)
-                list.add(movie)
-            }
-            if (cursor.count == 0) {
-                return null
+    fun updateData() {
+        if (database == null){
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
+            return
+        }
+
+        val values = ContentValues()
+        values.put("name", "mike")
+        values.put("age", "24")
+        values.put("mobile", "010-4000-4000")
+        var arr : Array<String> = arrayOf("john")
+        database?.update(tableName, values, "name=?", arr)
+        binding.output1.append("데이터 갱신\n")
+    }
+
+    //데이터 조회
+    fun queryData(){
+        if(database == null){
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
+            return
+        }
+        // TODO : 데이터 조회
+        val cursor = database?.rawQuery("select id, name, age, mobile from $tableName", null)
+        if (cursor != null) {
+            for (index in 0 until cursor.count) {
+                cursor.moveToNext()
+                val id = cursor.getInt(0)
+                val name = cursor.getString(1)
+                val age = cursor.getInt(2)
+                val mobile = cursor.getString(3)
+                binding.output1.append("레코드#${index} : $id, $name, $age, $mobile\n")
             }
             cursor.close()
         }
-        println("데이터 조회함\n")
-        return list
+        binding.output1.append("데이터 조회 결과\n")
+    }
+
+    fun deleteData() {
+        if(database == null){
+            binding.output1.append("데이터베이스를 먼저 오픈하세요.\n")
+            return
+        }
+
+        val sql = "select id, name, age, mobile from $tableName"
+        val cursor = database?.rawQuery(sql, null)
+        if (cursor != null) {
+            cursor.count
+            val count = cursor.count
+            cursor.close()
+
+            val delete = "delete from $tableName where id = ${count}"
+            database?.execSQL(delete)
+            binding.output1.append("데이터 삭제\n")
+        }
     }
 }
-
-data class ReservedMovie(
-    val _id:Int?,
-    val name:String?,
-    val poster_image:String?,
-    val director: String?,
-    val synopsis: String?,
-    val reserved_time: String?
-):Serializable
